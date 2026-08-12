ba49990adc0a44fbb2023ce20189b03baf7c5f2e
varinder-netzila <varinder@netzilatechnologies.com>
2026-08-12 18:29:40 +0530

    test glb file 3

--- src/components/BottleViewer.tsx
+++ src/components/BottleViewer.tsx
@@ -576,18 +576,18 @@ const BottleModel = ({
           selectedBottleType
         );
       });
-  const newColor =
-    selectedColor?.Body?.hex || "#FFFFFF";
-  scene.traverse((child: any) => {
-    console.log(child.name);
-    if (
-      child.isMesh &&
-      child.name === "Mesh_0"
-    ) {
-      child.material.color.set(newColor);
-      child.material.needsUpdate = true;
-    }
-  });     
+  // const newColor =
+  //   selectedColor?.Body?.hex || "#FFFFFF";
+  // scene.traverse((child: any) => {
+  //   console.log(child.name);
+  //   if (
+  //     child.isMesh &&
+  //     child.name === "Mesh_0"
+  //   ) {
+  //     child.material.color.set(newColor);
+  //     child.material.needsUpdate = true;
+  //   }
+  // });     
     }
 
     // Apply final composed texture ONLY to base body scene
