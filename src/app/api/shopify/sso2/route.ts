a72dcb681977baec1560e7ae4f2f34163d5e15d3
varinder-netzila <varinder@netzilatechnologies.com>
2026-09-21 20:06:30 +0530

    test proxy 2

---
+++ src/app/api/shopify/sso2/route.ts
@@ -0,0 +1,14 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+  const { searchParams } = req.nextUrl;
+  const params = Object.fromEntries(searchParams);
+
+  return new NextResponse(
+    `Hello from proxy. Params: ${JSON.stringify(params)}`,
+    {
+      status: 200,
+      headers: { "Content-Type": "text/plain" },
+    }
+  );
+}
