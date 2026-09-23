d409e9e94fb1c2a2e1687ca66c9d380d6e1e8927
varinder-netzila <varinder@netzilatechnologies.com>
2026-09-23 17:29:29 +0530

    fix jwt error 888

--- src/app/api/auth/logout/route.ts
+++ src/app/api/auth/logout/route.ts
@@ -11,7 +11,7 @@ export async function GET() {
   response.cookies.delete({
     name: SESSION_COOKIE,
     path: "https://marvinscloud.com/en/configurator",       // must match the path used when the cookie was set
-    // domain: "marvinscloud.com", // only if you explicitly set a domain originally
+    domain: "marvinscloud.com", // only if you explicitly set a domain originally
   });
 
   return response;
